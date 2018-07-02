8264f90c0ed139d3bfdf085865a30f0cc5e184f7
frasco2001 <frasco2001@hotmail.com>
2018-07-02 17:16:17 +0200

    Add files via upload

---
+++ app/models/articulos.js
@@ -0,0 +1,14 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var ArticulosSchema = new Schema({
+    nombre: { type: String },
+    precio: { type: Number },
+    codigo: { type: String },
+    compania: { type: String },
+    activo: { type: Boolean, default: 'TRUE' },
+    create_time: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Articulos', ArticulosSchema);
---
+++ app/models/companias.js
@@ -0,0 +1,11 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var CompaniasSchema = new Schema({
+    nombre: String,
+    activo: { type: Boolean, default: 'TRUE' },
+    create_time: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Compañias', CompaniasSchema);
